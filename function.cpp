094031acf5e59d44e8986c1bb793eccc771f1f92
DanHuang929 <67536310+DanHuang929@users.noreply.github.com>
2020-10-13 17:32:42 +0800

    Add files via upload
    10/13

--- function.cpp
+++ function.cpp
@@ -325,8 +325,8 @@ BLOCK::BLOCK(char type, int type_n, int start_point, int move)
 			map[this->box[3].row][this->box[3].col] = 1;
 		}
 		else if (type_n == 2) {
-			this->box[0].row = maxrow - row - 1;           //012
-			this->box[0].col = start_point;                // 3
+			this->box[0].row = maxrow - row - 1;           
+			this->box[0].col = start_point;                
 			map[this->box[0].row][this->box[0].col] = 1;
 
 			this->box[1].row = maxrow - row - 1;
@@ -363,15 +363,13 @@ BLOCK::BLOCK(char type, int type_n, int start_point, int move)
 
 void falling(BLOCK& block)
 {
-	while (valid_falling(block)) {
-		for (int i = 0; i < 4; i++) {
-			map[block.box[i].row][block.box[i].col] = 0;
-			block.box[i].row += 1;
-		}
-		for (int i = 0; i < 4; i++)
-			map[block.box[i].row ][block.box[i].col] = 1;
-
-	}
+	for (int i = 0; i < 4; i++)
+		map[block.box[i].row ][block.box[i].col] = 0;
+	while (valid_falling(block)) 
+		for (int i = 0; i < 4; i++) 
+		block.box[i].row += 1;
+	for (int i = 0; i < 4; i++)
+		map[block.box[i].row ][block.box[i].col] = 1;
 	int erase_target;
 	erase_target = detect_falling();
 	while (erase_target != 0) {
@@ -382,16 +380,9 @@ void falling(BLOCK& block)
 
 int valid_falling (BLOCK& block)
 {
-	for (int i = 0; i < 4; i++) {
-		if (map[block.box[i].row + 1][block.box[i].col] == 1 or (block.box[i].row + 1) > maxrow - 1) {
-			int self = 0;
-			for (int k = 0; k < 4; k++) 
-				if ((block.box[i].row + 1 == block.box[k].row) && (block.box[i].col == block.box[k].col)) 
-					self = 1;
-			if (self) continue;
-			else return 0;
-		}
-	}
+	for (int i = 0; i < 4; i++) 
+		if (map[block.box[i].row + 1][block.box[i].col] == 1 or (block.box[i].row + 1) > maxrow - 1) 
+			return 0;
 	return 1;
 }
 
@@ -407,11 +398,11 @@ int detect_falling(void)
 	}
 	return 0;
 } 
-//回傳需要被削除的row
+
 
 void erase(int target_row)
 {
-	for (int i = target_row; i >= maxrow - row - 4; i--) { //注意最top
+	for (int i = target_row; i >= maxrow - row - 4; i--) { 
 		for (int j = 1; j <= col; j++) {
 			map[i][j] = map[i - 1][j];
 		}
@@ -421,17 +412,16 @@ void erase(int target_row)
 int moving(BLOCK& block, int move)
 {
 	int step = (move > 0) ? 1 : -1;
+	for (int i = 0; i < 4; i++) 
+		map[block.box[i].row][block.box[i].col] = 0;
 	for (int i = 0; i < move * step; i++) {
-		if (valid_moving(block, step)) {
-			for (int i = 0; i < 4; i++) {
-				map[block.box[i].row][block.box[i].col] = 0;
+		if (valid_moving(block, step)) 
+			for (int i = 0; i < 4; i++) 
 				block.box[i].col += step;
-			}
-			for (int i = 0; i < 4; i++)
-				map[block.box[i].row][block.box[i].col] = 1;
-		}
 		else return 0;
 	}
+	for (int i = 0; i < 4; i++)
+		map[block.box[i].row][block.box[i].col] = 1;
 	return 1;
 }
 
@@ -441,13 +431,7 @@ int valid_moving(BLOCK& block, int step)
 		if (map[block.box[i].row][block.box[i].col + step] == 1
 			or (block.box[i].col + step) < 1
 			or (block.box[i].col + step) > col) {
-			int self = 0;
-			for (int k = 0; k < 4; k++)
-				if ((block.box[i].row == block.box[k].row) && (block.box[i].col + step == block.box[k].col))
-					self = 1;
-			cout << "ti\n";
-			if (self) continue;
-			else return 0;
+			return 0;
 		}
 	}
 	return 1;
@@ -460,10 +444,18 @@ void lose(void)
 
 void print(void)
 {
-	for (int i = maxrow - row - 4; i < maxrow; i++) {
-		cout << i << " ";
+	for (int i = maxrow - row; i < maxrow; i++) {
 		for (int j = 1; j <= col; j++)
-			cout << map[i][j];
+			cout << map[i][j]<<" ";
 		cout << "\n";
 	}
+}
+
+int test(void)
+{
+	for (int i = 1; i <= col; i++) {
+		if (map[maxrow - row - 1][i] == 1)
+			return 1;
+	}
+	return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include <stdio.h>
+#include<fstream>
+#include<sstream>
 #include "function.h"
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-	cin >> row >> col;
+	char a[10];
+	ifstream fin;
+	fin.open(argv[1]);
+	fin.getline(a, 10);
+	stringstream str(a);
+	str>> row >> col;
 	char type;
 	int type_n = 0;
 	int start_point, move;
+	char oper[20];
 	for (int i = 0; i < maxrow; i++)
 		for (int j = 0; j < col; j++)
 			map[i][j] = 0;
-	while (cin >> type) {
+	while (!fin.eof()) {
+		fin.getline(oper, 10);
+		cout << oper << endl;
+		stringstream ss(oper);
+		ss >> type;
 		if (type == 'E')
 			break;
 		else {
 			if (type != 'O')
-				cin >> type_n >> start_point >> move;
+				ss >> type_n >> start_point >> move;
 			else {
-				cin >> start_point >> move;
+				ss >> start_point >> move;
 				type_n = 0;
 			}
 			BLOCK block(type, type_n, start_point, move);
@@ -28,9 +40,15 @@ int main()
 				lose();
 				break;
 			}
+			falling(block);
+			if (test()) {
+				lose();
+				break;
+			}
 		}
 		print();
-
+		ofstream fout;
+		//fout.open("C:\Users\黃允暘\Desktop")
 		
 	}
 }
